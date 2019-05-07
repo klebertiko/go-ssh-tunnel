6ededb28fff05163f963b052a3cf30c08ed037de
Kleber Almeida Toyota <klebertiko@gmail.com>
2019-05-07 11:56:07 -0300

    initial

---
+++ cmd/go-ssh-tunnel-other.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type Endpoint struct {
+	Host string
+	Port int
+}
+
+func (endpoint *Endpoint) String() string {
+	return fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port)
+}
+
+// From https://sosedoff.com/2015/05/25/ssh-port-forwarding-with-go.html
+// Handle local client connections and tunnel data to the remote server
+// Will use io.Copy - http://golang.org/pkg/io/#Copy
+func handleClient(client net.Conn, remote net.Conn) {
+	defer client.Close()
+	chDone := make(chan bool)
+
+	// Start remote -> local data transfer
+	go func() {
+		_, err := io.Copy(client, remote)
+		if err != nil {
+			log.Println(fmt.Sprintf("error while copy remote->local: %s", err))
+		}
+		chDone <- true
+	}()
+
+	// Start local -> remote data transfer
+	go func() {
+		_, err := io.Copy(remote, client)
+		if err != nil {
+			log.Println(fmt.Sprintf("error while copy local->remote: %s", err))
+		}
+		chDone <- true
+	}()
+
+	<-chDone
+}
+
+// local service to be forwarded
+var localEndpoint = Endpoint{
+	Host: "localhost",
+	Port: 8080,
+}
+
+// remote SSH server
+var serverEndpoint = Endpoint{
+	Host: "10.130.13.96",
+	Port: 22,
+}
+
+// remote forwarding port (on remote SSH server network)
+var remoteEndpoint = Endpoint{
+	Host: "localhost",
+	Port: 8080,
+}
+
+func main() {
+	// refer to https://godoc.org/golang.org/x/crypto/ssh for other authentication types
+	sshConfig := &ssh.ClientConfig{
+		User: "kleber.toyota",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("meCvnhH1i"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// Connect to SSH remote server using serverEndpoint
+	serverConn, err := ssh.Dial("tcp", serverEndpoint.String(), sshConfig)
+	if err != nil {
+		log.Fatalln(fmt.Printf("Dial INTO remote server error: %s", err))
+	}
+
+	// Listen on remote server port
+	listener, err := serverConn.Listen("tcp", remoteEndpoint.String())
+	if err != nil {
+		log.Fatalln(fmt.Printf("Listen open port ON remote server error: %s", err))
+	}
+	defer listener.Close()
+
+	// handle incoming connections on reverse forwarded tunnel
+	for {
+		// Open a (local) connection to localEndpoint whose content will be forwarded so serverEndpoint
+		local, err := net.Dial("tcp", localEndpoint.String())
+		if err != nil {
+			log.Fatalln(fmt.Printf("Dial INTO local service error: %s", err))
+		}
+
+		client, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		handleClient(client, local)
+	}
+}
---
+++ cmd/go-ssh-tunnel.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type Endpoint struct {
+	Host string
+	Port int
+}
+
+func (endpoint *Endpoint) String() string {
+	return fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port)
+}
+
+type SSHtunnel struct {
+	Local  *Endpoint
+	Server *Endpoint
+	Remote *Endpoint
+
+	Config *ssh.ClientConfig
+}
+
+func (tunnel *SSHtunnel) Start() error {
+	listener, err := net.Listen("tcp", tunnel.Local.String())
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go tunnel.forward(conn)
+	}
+}
+
+func (tunnel *SSHtunnel) forward(localConn net.Conn) {
+	serverConn, err := ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		return
+	}
+
+	remoteConn, err := serverConn.Dial("tcp", tunnel.Remote.String())
+	if err != nil {
+		fmt.Printf("Remote dial error: %s\n", err)
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Printf("io.Copy error: %s", err)
+		}
+	}
+
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+}
+
+func main() {
+	user := flag.String("user", "", "SSH username\n\tEG.: user")
+	pass := flag.String("pass", "", "SSH password\n\tEg.: 1234")
+
+	local := flag.String("local", "", "Local address to bind remote service\n\tEg.: localhost")
+	localPort := flag.Int("localPort", 0, "Local port to bind remote service\n\tEg.: 3000")
+
+	sshServer := flag.String("sshServer", "", "SSH server address\n\tEg.: 10.1.1.2")
+
+	remote := flag.String("remote", "", "Remote service address\n\tEg.: localhost")
+	remotePort := flag.Int("remotePort", 0, "Remote service port\n\tEg.: 3000")
+
+	flag.Parse()
+	if *user == "" || *pass == "" || *local == "" || *localPort == 0 || *sshServer == "" || *remote == "" || *remotePort == 0 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User: *user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(*pass),
+		},
+	}
+
+	localEndpoint := &Endpoint{
+		Host: *local,
+		Port: *localPort,
+	}
+
+	serverEndpoint := &Endpoint{
+		Host: *sshServer,
+		Port: 22,
+	}
+
+	remoteEndpoint := &Endpoint{
+		Host: *remote,
+		Port: *remotePort,
+	}
+
+	tunnel := &SSHtunnel{
+		Config: sshConfig,
+		Local:  localEndpoint,
+		Server: serverEndpoint,
+		Remote: remoteEndpoint,
+	}
+	go tunnel.Start()
+}
